b9e640717b0ad04542574ed128d9fb74fb4fe5f7
SukhilNairEEN <sukhil@een.com>
2024-09-01 19:31:42 +0530

    Update Added shell script to run the flask in remove machine.

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo apt-get install screen -y
+sudo kill -9 `ps aux | grep app.py | awk '{print $2}'`
+SESSION_NAME="flask_app"
+FLASK_COMMAND="python3 /home/ubuntu/app.py"
+
+if ! command -v tmux &> /dev/null
+then
+    echo "tmux could not be found, installing it..."
+    sudo apt-get update
+    sudo apt-get install tmux
+fi
+tmux new -d -s $SESSION_NAME $FLASK_COMMAND
+
+if tmux ls | grep -q "$SESSION_NAME"; then
+  echo "Flask app is running in a tmux session named $SESSION_NAME."
+else
+  echo "Failed to start Flask app in a tmux session."
+fi
